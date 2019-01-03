a5c4a79d97ec52aae9cd199ee38dd162f9adf789
Ali <ali.rivera@thalamuscorp.com>
2019-01-03 01:44:43 -0300

    mobile search

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
-function myFunction(){
+function toggleNavbar(){
     var navMobile = document.getElementById("nav__mobile");
     navMobile.classList.toggle("nav__mobile--open");
+}
+
+function toggleSearch(){
+    var navSearch = document.getElementById("nav__search");
+    navSearch.classList.toggle("nav__search--open");
 }
